d216b3530e5e6c9832c18c68700377ac97dd59ca
juanjomoga <juanjomoga@gmail.com>
2017-11-14 13:06:13 +0100

    Páginas Estáticas

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,8 +1,11 @@
 class EstaticasController < ApplicationController
   def contacto
+  	@mensaje = "Hola desde el controller"
+  	@usuarios = ['Pablo','María','Silvia', 'Daniel']
   end
 
   def nosotros
+  	
   end
 
 end
